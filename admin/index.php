88877a6353c1fd6bd7c9bd977845a3b290b53f5d
ThaiCoder2003 <quocthai290103@gmail.com>
2023-06-23 21:09:39 +0700

    Completed Basic Username/Password Box

--- admin/index.php
+++ admin/index.php
@@ -9,10 +9,19 @@
 </head>
 <body class='bg-light'>
     <div>
-        <form>
+        <form class="form">
             <h4>Admin Login</h4>
+            <div>
+                <div class="username">
+                    <input type="username" class="form-control shadow_none text-center" placeholder="Enter Admin's Username">
+                </div>
+                <div class="password">
+                    <input type="password" class="form-control shadow_none text-center" placeholder="Enter Admin's Password">
+                </div>
+            </div>
         </form>
     </div>
     
+    <?php require('inc/scripts.php'); ?>
 </body>
 </html>
